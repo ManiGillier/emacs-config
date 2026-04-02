b72231a8c69a10a97f04d8c6ee56af7a61f0a988
Mani Gillier <mani.gillier@openindus.com>
2026-04-02 08:40:11 +0200

    [Feat] Add init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+(load "~/.emacs.d/config.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/mani/org/notes.org" "/home/mani/org/organization.org"))
+ '(package-selected-packages
+   '(org-alert company forge ivy magit magit-gitlab magit-lfs treemacs)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
